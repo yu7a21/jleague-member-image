8ad60897e1d1a97c3c82a48c70ab11a66a8d3d9e
yu7aNakamura <nakamurayuta@team-lab.com>
2021-11-06 15:44:56 +0900

    add:modelつくった

--- jleague-api/app.py
+++ jleague-api/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config.db_config import init_db
 from config import config
 from model.team import Team
+from model.player import Player
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{config.db_user}:{config.db_password}@{config.db_host}/{config.db_database}?charset=utf8"
@@ -9,8 +10,9 @@ db = init_db(app)
 
 @app.route('/hello')
 def hello():
-    result = Team.query.all()
-    list = [t.name for t in result]
+    spulse_id = Team.query.filter_by(name="徳島ヴォルティス").first().id
+    result = Player.query.filter_by(team_id=spulse_id).all()
+    list = [f"{t.number}:{t.name_ja}" for t in result]
     return f"清水エスパルス：{list}"
 
 if __name__ == '__main__':
--- jleague-api/model/team.py
+++ jleague-api/model/team.py
@@ -5,6 +5,3 @@ class Team(db.Model):
     name = db.Column(db.String(50), nullable=False)
     short_name = db.Column(db.String(50), nullable=False)
     league = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self):
-        return '<Team %r>' % self.name
